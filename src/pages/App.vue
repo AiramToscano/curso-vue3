f358f2d8a8f26b44b005fcdbb6cfa191f410964b
airamtoscano <airam.almeida@ipdec.org>
2023-09-27 15:06:58 -0400

    Feat: adicionando componentes

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
-
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Componente from '@/components/PrimeiroComponente/Componente.vue'
 </script>
 
 <template>
   <section class="bg-gray-950 font-thin text-gray-300">
     hdhdhdhdhddh
-    <HelloWorld/>
-  </section>  
+    <HelloWorld />
+  </section>
+  <Componente />
 </template>
-
-
